aa1d54ffb56588171d4063651f5ce9200cc907a9
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2016-12-24 21:01:44 +1300

    Adding family exercise

---
+++ family.pl
@@ -0,0 +1,20 @@
+
+male(jerry).
+male(stuart).
+male(warren).
+female(kathe).
+female(maryalice).
+brother(jerry,stuart).
+brother(jerry,kathe).
+sister(kather,jerry).
+parent_of(warren,jerry).
+parent_of(maryalice,jerry).
+
+father(F,S):-
+  male(F),
+  parent_of(F,S).
+
+mother(M,S):-
+  female(M),
+  parent_of(M,S).
+
